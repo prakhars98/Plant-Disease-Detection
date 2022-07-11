d6c46bd0041804c4b1b3cc24156a8446dac349f9
prakhars98 <43076632+prakhars98@users.noreply.github.com>
2022-07-11 21:48:24 +0530

    Update keys.js

--- Plant Disease  Detection/App/config/keys.js
+++ Plant Disease  Detection/App/config/keys.js
@@ -1,3 +1,3 @@
 module.exports = {
-    MongoURI :'mongodb+srv://prakhar:prakhar%40123@cluster0.b4bpj.mongodb.net/?retryWrites=true&w=majority'
+    MongoURI :'your mongodb atlas URI displayed on the mongo platform'
 }
